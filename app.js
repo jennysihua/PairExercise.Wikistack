dc8a469527e24c374d7e09f6c480c8b7d8162500
Sean Lee <seanslee92@gmail.com>
2018-04-27 15:40:04 -0400

    second commit

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const models = require('./models');
 const express = require('express');
-
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
 const app = express();
 const wikiRouter = require('./routes/wiki.js')
 const userRouter = require('./routes/user.js')
 
 app.use('/wiki', wikiRouter)
 app.use('/user', userRouter)
-
+app.use(bodyParser.json())
+app.use(morgan('dev'))
 
 const PORT = 3000;
 
@@ -20,4 +22,6 @@ const init = async() => {
 
 init();
 
-
+app.get('/', (req, res, next) => {
+    res.redirect('/wiki');
+  })
--- routes/wiki.js
+++ routes/wiki.js
@@ -1,17 +1,28 @@
 const express = require('express')
 // const models = require('../models');
 const router = express.Router()
+const bodyParser = require('body-parser')
+const { addPage } = require('../views')
+const { Page } = require("../models")
 // const mainView = require('../views/main.js')
 module.exports = router
 
+router.use(bodyParser.json())
+
 router.get('/', (req, res, next) => {
-  res.send('Get eventually')
+  res.send('hi!')
 })
 
-router.post('/', (req, res, next) => {
-  res.send('Post eventually')
+router.post('/', async (req, res, next) => {
+  console.log(req.body)
+  // let postedPage = Page.create({ title: req.body.title,
+  //               content: req.body.content,
+  //               status: req.body.status,
+  //               createdAt: new Date(),
+  //               updatedAt: new Date()})
+
 })
 
 router.get('/add', (req, res, next) => {
-  res.send('Get add eventually')
+  res.send(addPage())
 })
